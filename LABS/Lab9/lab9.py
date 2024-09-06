340849f626e62a81a47b748b5623f678999cb2ef
Marco Donnarumma <126296999+donnadamus@users.noreply.github.com>
2024-09-07 00:54:29 +0200

    start lab9

---
+++ LABS/Lab9/lab9.py
@@ -0,0 +1,34 @@
+import numpy
+import scipy.special
+import sklearn.datasets
+
+def vcol(x):
+    return x.reshape((x.size, 1))
+
+def vrow(x):
+    return x.reshape((1, x.size))
+
+def split_db_2to1(D, L, seed=0):
+    
+    nTrain = int(D.shape[1]*2.0/3.0)
+    numpy.random.seed(seed)
+    idx = numpy.random.permutation(D.shape[1])
+    idxTrain = idx[0:nTrain]
+    idxTest = idx[nTrain:]
+    
+    DTR = D[:, idxTrain]
+    DVAL = D[:, idxTest]
+    LTR = L[idxTrain]
+    LVAL = L[idxTest]
+    return (DTR, LTR), (DVAL, LVAL)
+
+def load_iris_binary():
+    D, L = sklearn.datasets.load_iris()['data'].T, sklearn.datasets.load_iris()['target']    
+    D = D[:, L != 0] # We remove setosa from D
+    L = L[L!=0] # We remove setosa from L
+    L[L==2] = 0 # We assign label 0 to virginica (was label 2)
+    return D, L
+
+import bayesRisk
+
+if __name__ == '__main__':
