8da43b3fee8850d102494f42c038b20e708a08c5
Marco Donnarumma <126296999+donnadamus@users.noreply.github.com>
2024-09-07 22:17:46 +0200

    project lab 9 wip

---
+++ LABS/Lab9/Project/projectlab9.py
@@ -0,0 +1,161 @@
+import numpy as np, numpy
+import scipy.special
+import sklearn.datasets
+import bayesRisk
+
+
+def vcol(x):
+    return x.reshape((x.size, 1))
+
+def vrow(x):
+    return x.reshape((1, x.size))
+
+def load(fname, firstfeature=0, lastfeature=6):
+    DAttrs = [] # values for dataset features
+    DClass = [] # class for each row of the dataset
+
+    with open(fname) as f:
+        for line in f:
+            try:
+                attrs = line.split(',')[firstfeature:lastfeature]
+                attrs = vcol(np.array([float(i) for i in attrs]))
+                binaryClass = int(line.split(',')[-1].strip())
+                DAttrs.append(attrs)
+                DClass.append(binaryClass)
+            except:
+                pass
+
+    return np.hstack(DAttrs), np.array(DClass, dtype=np.int32)
+
+def split_db_2to1(D, L, seed=0):
+    
+    nTrain = int(D.shape[1]*2.0/3.0)
+    numpy.random.seed(seed)
+    idx = numpy.random.permutation(D.shape[1])
+    idxTrain = idx[0:nTrain]
+    idxTest = idx[nTrain:]
+    
+    DTR = D[:, idxTrain]
+    DVAL = D[:, idxTest]
+    LTR = L[idxTrain]
+    LVAL = L[idxTest]
+    return (DTR, LTR), (DVAL, LVAL)
+
+def load_iris_binary():
+    D, L = sklearn.datasets.load_iris()['data'].T, sklearn.datasets.load_iris()['target']    
+    D = D[:, L != 0] # We remove setosa from D
+    L = L[L!=0] # We remove setosa from L
+    L[L==2] = 0 # We assign label 0 to virginica (was label 2)
+    return D, L
+
+# Optimize SVM
+def train_dual_SVM_linear(DTR, LTR, C, K = 1):
+    
+    ZTR = LTR * 2.0 - 1.0 # Convert labels to +1/-1
+    DTR_EXT = numpy.vstack([DTR, numpy.ones((1,DTR.shape[1])) * K])
+    H = numpy.dot(DTR_EXT.T, DTR_EXT) * vcol(ZTR) * vrow(ZTR)
+
+    # Dual objective with gradient
+    def fOpt(alpha):
+        Ha = H @ vcol(alpha)
+        loss = 0.5 * (vrow(alpha) @ Ha).ravel() - alpha.sum()
+        grad = Ha.ravel() - numpy.ones(alpha.size)
+        return loss, grad
+
+    alphaStar, _, _ = scipy.optimize.fmin_l_bfgs_b(fOpt, numpy.zeros(DTR_EXT.shape[1]), bounds = [(0, C) for i in LTR], factr=1.0)
+    
+    # Primal loss
+    def primalLoss(w_hat):
+        S = (vrow(w_hat) @ DTR_EXT).ravel()
+        return 0.5 * numpy.linalg.norm(w_hat)**2 + C * numpy.maximum(0, 1 - ZTR * S).sum()
+
+    # here we convert to primal solution
+    # Compute primal solution for extended data matrix
+    w_hat = (vrow(alphaStar) * vrow(ZTR) * DTR_EXT).sum(1)
+    
+    # Extract w and b - alternatively, we could construct the extended matrix for the samples to score and use directly v
+    w, b = w_hat[0:DTR.shape[0]], w_hat[-1] * K # b must be rescaled in case K != 1, since we want to compute w'x + b * K
+
+    primalLoss, dualLoss = primalLoss(w_hat), -fOpt(alphaStar)[0]
+    print ('SVM - C %e - K %e - primal loss %e - dual loss %e - duality gap %e' % (C, K, primalLoss, dualLoss, primalLoss - dualLoss))
+    
+    return w, b
+
+# We create the kernel function. Since the kernel function may need additional parameters, we create a function that creates on the fly the required kernel function
+# The inner function will be able to access the arguments of the outer function
+def polyKernel(degree, c):
+    
+    def polyKernelFunc(D1, D2):
+        return (numpy.dot(D1.T, D2) + c) ** degree
+
+    return polyKernelFunc
+
+def rbfKernel(gamma):
+
+    def rbfKernelFunc(D1, D2):
+        # Fast method to compute all pair-wise distances. Exploit the fact that |x-y|^2 = |x|^2 + |y|^2 - 2 x^T y, combined with broadcasting
+        D1Norms = (D1**2).sum(0)
+        D2Norms = (D2**2).sum(0)
+        Z = vcol(D1Norms) + vrow(D2Norms) - 2 * numpy.dot(D1.T, D2)
+        return numpy.exp(-gamma * Z)
+
+    return rbfKernelFunc
+
+# kernelFunc: function that computes the kernel matrix from two data matrices
+def train_dual_SVM_kernel(DTR, LTR, C, kernelFunc, eps = 1.0):
+
+    ZTR = LTR * 2.0 - 1.0 # Convert labels to +1/-1
+    K = kernelFunc(DTR, DTR) + eps
+    H = vcol(ZTR) * vrow(ZTR) * K
+
+    # Dual objective with gradient
+    def fOpt(alpha):
+        Ha = H @ vcol(alpha)
+        loss = 0.5 * (vrow(alpha) @ Ha).ravel() - alpha.sum()
+        grad = Ha.ravel() - numpy.ones(alpha.size)
+        return loss, grad
+
+    alphaStar, _, _ = scipy.optimize.fmin_l_bfgs_b(fOpt, numpy.zeros(DTR.shape[1]), bounds = [(0, C) for i in LTR], factr=1.0)
+
+    print ('SVM (kernel) - C %e - dual loss %e' % (C, -fOpt(alphaStar)[0]))
+
+    # Function to compute the scores for samples in DTE
+    def fScore(DTE):
+        
+        K = kernelFunc(DTR, DTE) + eps
+        H = vcol(alphaStar) * vcol(ZTR) * K
+        return H.sum(0)
+
+    return fScore # we directly return the function to score a matrix of test samples
+
+if __name__ == '__main__':
+    
+    dataset, labels = load("/Users/marcodonnarumma/Desktop/MLPR/MLPR/LABS/Lab9/Project/trainData.txt")
+
+    (DTR, LTR), (DVAL, LVAL) = split_db_2to1(dataset, labels)
+
+    # use fraction of data
+
+    DTR = DTR[::50]
+    LTR = LTR[::50]
+
+    C_vals = np.logspace(-5, 0, 11) # from 10^-5 to 10^11
+
+    K = 1.0
+
+    # K is the regularization term for the bias (b)
+
+    effPrior = 0.1
+
+    # C is a regularization parameter that controls the trade-off between achieving a low training
+    # error and a low testing error, which affects the generalization of the model
+
+    # If C is high, we can say that we are adding high penalty to missclassifications.
+    # The model will try to correctly classify as many training points as possible. As we can imagine, less regularization mean we
+    # could have high overfitting.
+
+    # If C is low, we can say that we are not adding much penalty to missclassifications.
+    # It allows us to have a model that generalizes better, even if that means having some missclassified point
+
+    for C in C_vals:
+
