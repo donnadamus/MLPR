090eb2d8860bbd7ebe42228dbc27c97c7268d956
Marco Donnarumma <126296999+donnadamus@users.noreply.github.com>
2024-09-07 18:53:58 +0200

    finished lab9, will now start project

--- LABS/Lab9/lab9.py
+++ LABS/Lab9/lab9.py
@@ -64,16 +64,75 @@ def train_dual_SVM_linear(DTR, LTR, C, K = 1):
     
     return w, b
 
+# We create the kernel function. Since the kernel function may need additional parameters, we create a function that creates on the fly the required kernel function
+# The inner function will be able to access the arguments of the outer function
+def polyKernel(degree, c):
+    
+    def polyKernelFunc(D1, D2):
+        return (numpy.dot(D1.T, D2) + c) ** degree
+
+    return polyKernelFunc
+
+def rbfKernel(gamma):
+
+    def rbfKernelFunc(D1, D2):
+        # Fast method to compute all pair-wise distances. Exploit the fact that |x-y|^2 = |x|^2 + |y|^2 - 2 x^T y, combined with broadcasting
+        D1Norms = (D1**2).sum(0)
+        D2Norms = (D2**2).sum(0)
+        Z = vcol(D1Norms) + vrow(D2Norms) - 2 * numpy.dot(D1.T, D2)
+        return numpy.exp(-gamma * Z)
+
+    return rbfKernelFunc
+
+# kernelFunc: function that computes the kernel matrix from two data matrices
+def train_dual_SVM_kernel(DTR, LTR, C, kernelFunc, eps = 1.0):
+
+    ZTR = LTR * 2.0 - 1.0 # Convert labels to +1/-1
+    K = kernelFunc(DTR, DTR) + eps
+    H = vcol(ZTR) * vrow(ZTR) * K
+
+    # Dual objective with gradient
+    def fOpt(alpha):
+        Ha = H @ vcol(alpha)
+        loss = 0.5 * (vrow(alpha) @ Ha).ravel() - alpha.sum()
+        grad = Ha.ravel() - numpy.ones(alpha.size)
+        return loss, grad
+
+    alphaStar, _, _ = scipy.optimize.fmin_l_bfgs_b(fOpt, numpy.zeros(DTR.shape[1]), bounds = [(0, C) for i in LTR], factr=1.0)
+
+    print ('SVM (kernel) - C %e - dual loss %e' % (C, -fOpt(alphaStar)[0]))
+
+    # Function to compute the scores for samples in DTE
+    def fScore(DTE):
+        
+        K = kernelFunc(DTR, DTE) + eps
+        H = vcol(alphaStar) * vcol(ZTR) * K
+        return H.sum(0)
+
+    return fScore # we directly return the function to score a matrix of test samples
+
+
 if __name__ == '__main__':
     D, L = load_iris_binary()
     (DTR, LTR), (DVAL, LVAL) = split_db_2to1(D, L)
+    for K in [1, 10]:
+        for C in [0.1, 1.0, 10.0]:
+            w, b = train_dual_SVM_linear(DTR, LTR, C, K)
+            SVAL = (vrow(w) @ DVAL + b).ravel()
+            PVAL = (SVAL > 0) * 1
+            err = (PVAL != LVAL).sum() / float(LVAL.size)
+            print ('Error rate: %.1f' % (err*100))
+            print ('minDCF - pT = 0.5: %.4f' % bayesRisk.compute_minDCF_binary_fast(SVAL, LVAL, 0.5, 1.0, 1.0))
+            print ('actDCF - pT = 0.5: %.4f' % bayesRisk.compute_actDCF_binary_fast(SVAL, LVAL, 0.5, 1.0, 1.0))
+            print ()
 
-    for (K, C) in [(1, 0.1), (1, 1.0), (1, 10.0), (10, 0.1), (10, 1.0), (10, 10.0)]:
-        w, b = train_dual_SVM_linear(DTR, LTR, C, K)
-        SVAL = (vrow(w) @ DVAL + b).ravel()
-        PVAL = (SVAL > 0) * 1
-        err = (PVAL != LVAL).sum() / float(LVAL.size)
-        print ('Error rate: %.1f' % (err*100))
-        print ('minDCF - pT = 0.5: %.4f' % bayesRisk.compute_minDCF_binary_fast(SVAL, LVAL, 0.5, 1.0, 1.0))
-        print ('actDCF - pT = 0.5: %.4f' % bayesRisk.compute_actDCF_binary_fast(SVAL, LVAL, 0.5, 1.0, 1.0))
-        print ()
+    for kernelFunc in [polyKernel(2, 0), polyKernel(2, 1), rbfKernel(1.0), rbfKernel(10.0)]:
+        for eps in [0.0, 1.0]:
+            fScore = train_dual_SVM_kernel(DTR, LTR, 1.0, kernelFunc, eps)
+            SVAL = fScore(DVAL)
+            PVAL = (SVAL > 0) * 1
+            err = (PVAL != LVAL).sum() / float(LVAL.size)
+            print ('Error rate: %.1f' % (err*100))
+            print ('minDCF - pT = 0.5: %.4f' % bayesRisk.compute_minDCF_binary_fast(SVAL, LVAL, 0.5, 1.0, 1.0))
+            print ('actDCF - pT = 0.5: %.4f' % bayesRisk.compute_actDCF_binary_fast(SVAL, LVAL, 0.5, 1.0, 1.0))
+            print ()
