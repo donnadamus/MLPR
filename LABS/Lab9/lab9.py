d5bf7aa399eac8a0f413b22c45815e0e7496ea7f
Marco Donnarumma <126296999+donnadamus@users.noreply.github.com>
2024-09-07 03:53:04 +0200

    lab9 wip

--- LABS/Lab9/lab9.py
+++ LABS/Lab9/lab9.py
@@ -1,6 +1,8 @@
 import numpy
 import scipy.special
 import sklearn.datasets
+import bayesRisk
+
 
 def vcol(x):
     return x.reshape((x.size, 1))
@@ -29,6 +31,49 @@ def load_iris_binary():
     L[L==2] = 0 # We assign label 0 to virginica (was label 2)
     return D, L
 
-import bayesRisk
+# Optimize SVM
+def train_dual_SVM_linear(DTR, LTR, C, K = 1):
+    
+    ZTR = LTR * 2.0 - 1.0 # Convert labels to +1/-1
+    DTR_EXT = numpy.vstack([DTR, numpy.ones((1,DTR.shape[1])) * K])
+    H = numpy.dot(DTR_EXT.T, DTR_EXT) * vcol(ZTR) * vrow(ZTR)
+
+    # Dual objective with gradient
+    def fOpt(alpha):
+        Ha = H @ vcol(alpha)
+        loss = 0.5 * (vrow(alpha) @ Ha).ravel() - alpha.sum()
+        grad = Ha.ravel() - numpy.ones(alpha.size)
+        return loss, grad
+
+    alphaStar, _, _ = scipy.optimize.fmin_l_bfgs_b(fOpt, numpy.zeros(DTR_EXT.shape[1]), bounds = [(0, C) for i in LTR], factr=1.0)
+    
+    # Primal loss
+    def primalLoss(w_hat):
+        S = (vrow(w_hat) @ DTR_EXT).ravel()
+        return 0.5 * numpy.linalg.norm(w_hat)**2 + C * numpy.maximum(0, 1 - ZTR * S).sum()
+
+    # here we convert to primal solution
+    # Compute primal solution for extended data matrix
+    w_hat = (vrow(alphaStar) * vrow(ZTR) * DTR_EXT).sum(1)
+    
+    # Extract w and b - alternatively, we could construct the extended matrix for the samples to score and use directly v
+    w, b = w_hat[0:DTR.shape[0]], w_hat[-1] * K # b must be rescaled in case K != 1, since we want to compute w'x + b * K
+
+    primalLoss, dualLoss = primalLoss(w_hat), -fOpt(alphaStar)[0]
+    print ('SVM - C %e - K %e - primal loss %e - dual loss %e - duality gap %e' % (C, K, primalLoss, dualLoss, primalLoss - dualLoss))
+    
+    return w, b
 
 if __name__ == '__main__':
+    D, L = load_iris_binary()
+    (DTR, LTR), (DVAL, LVAL) = split_db_2to1(D, L)
+
+    for (K, C) in [(1, 0.1), (1, 1.0), (1, 10.0), (10, 0.1), (10, 1.0), (10, 10.0)]:
+        w, b = train_dual_SVM_linear(DTR, LTR, C, K)
+        SVAL = (vrow(w) @ DVAL + b).ravel()
+        PVAL = (SVAL > 0) * 1
+        err = (PVAL != LVAL).sum() / float(LVAL.size)
+        print ('Error rate: %.1f' % (err*100))
+        print ('minDCF - pT = 0.5: %.4f' % bayesRisk.compute_minDCF_binary_fast(SVAL, LVAL, 0.5, 1.0, 1.0))
+        print ('actDCF - pT = 0.5: %.4f' % bayesRisk.compute_actDCF_binary_fast(SVAL, LVAL, 0.5, 1.0, 1.0))
+        print ()
